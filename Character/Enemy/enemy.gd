5aca58fe4c422429d81870f41d62cf12714ea92c
CaptainTiki <cpttiki@gmail.com>
2025-08-14 06:55:33 -0400

    Format wander state

--- Character/Enemy/enemy.gd
+++ Character/Enemy/enemy.gd
@@ -3,6 +3,7 @@ class_name Enemy
 
 @onready var health_component: HealthComponent = get_node_or_null("HealthComponent")
 @onready var state_component: StateComponent = get_node_or_null("StateComponent")
+@onready var rotation_handle: Node3D = get_node_or_null("RotationHandle")
 
 func _ready() -> void:
 	add_to_group("enemies")
@@ -31,9 +32,13 @@ func take_damage(amount: int) -> void:
 		health_component.take_damage(amount)
 
 func attack(target: Player) -> void:
-	var target_health: HealthComponent = target.get_node_or_null("HealthComponent")
-	if target_health:
-		target_health.take_damage(1)
+        var target_health: HealthComponent = target.get_node_or_null("HealthComponent")
+        if target_health:
+                target_health.take_damage(1)
 
 func _on_died() -> void:
-	queue_free()
+        queue_free()
+
+func set_facing_right(facing_right: bool) -> void:
+        if rotation_handle:
+                rotation_handle.rotation.y = 0 if facing_right else PI
